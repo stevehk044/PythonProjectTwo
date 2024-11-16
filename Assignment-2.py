16accf6873b7247507aa037bdc20e9b33d6a90a8
stevehk044 <stevehk044@gmail.com>
2024-11-15 22:05:00 -0500

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,8 @@
+# Ask the user to enter the length of the rectangle
+length = float(input("Enter the length of the rectangle: "))
+# Ask the user to enter the width of the rectangle
+width = float(input("Enter the width of the rectangle: "))
+# Calculate the area of the rectangle
+area = length * width
+# Display the calculated area
+print(f" The area of the rectangle is: {area}")
